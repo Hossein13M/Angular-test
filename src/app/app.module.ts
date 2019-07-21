4888bab137a28d9f700813834d8030409e5ef0a5
Hossein Mousavi <hossein13mousavi@gmail.com>
2019-07-21 22:49:11 +0430

    working with the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
