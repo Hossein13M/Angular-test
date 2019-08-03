ed64d4de66c4fb0c87d93c7b75f268a998b473e6
Hossein Mousavi <hossein13mousavi@gmail.com>
2019-08-03 22:12:31 +0430

    fresh start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
